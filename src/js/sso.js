03bee970d86e8238f5f8538f8fc13347fdec1892
Alex Scown <alex.scown@hp.com>
2015-07-20 12:58:48 +0100

    Document sso.js [rev: matthew.gordon]

--- src/js/sso.js
+++ src/js/sso.js
@@ -1,3 +1,23 @@
+/**
+ * @typedef {Object} SsoConfig
+ * @property {string} ssoEntryPage The page which is using this script
+ * @property {string} errorPage The error page to redirect to if there is an error. This page will be passed a
+ * statusCode query parameter
+ * @property {string} [ssoPage] The URL of the HP Haven OnDemand SSO page
+ * @property {SignedRequest} [listApplicationRequest] The request to be made to list the available applications
+ */
+/**
+ * Script for obtaining a combined token from HP Haven OnDemand and sending it to your server. This script will read the
+ * contents of the element with id 'config-json' and use it as its configuration. A script tag with type
+ * 'application/json' is best used for this purpose.
+ *
+ * Once authentication has occurred the components of the token
+ * received from HP Haven OnDemand will be appended to the form with id 'authenticate-form', and this form will be
+ * submitted, allowing your application to use the combined token. This form must exist on the document and should
+ * already contain any other inputs you need to submit to your server.
+ *
+ * The configuration should of type SsoConfig.
+ */
 (function() {
     var CONFIG = JSON.parse(document.getElementById('config-json').textContent);
     var applicationRoot = location.pathname.substring(0, location.pathname.lastIndexOf(CONFIG.ssoEntryPage));
